0359d20dc372328a87d4bd69cf6fbfdb1a3865c1
jujuReggio <juli.reggio@gmail.com>
2025-11-15 00:24:48 -0300

    v. 3

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     const $lightBtn = $('#light-theme');
     const $darkBtn = $('#dark-theme');
 
-    // Cargar tema guardado
     const savedTheme = localStorage.getItem('theme') || 'light';
     $body.attr('data-theme', savedTheme);
 
@@ -37,6 +36,42 @@ $(document).ready(function () {
         localStorage.setItem('font', newFont);
     });
 
+
+    // CAMBIO DE TAMAÑO DE LETRA
+
+    const MIN_FONT_SIZE = 12;
+    const MAX_FONT_SIZE = 20;
+    const DEFAULT_FONT_SIZE = 16;
+
+    const $btnIncrease = $('#font-increase');
+    const $btnDecrease = $('#font-decrease');
+
+
+    let savedFontSize = parseInt(localStorage.getItem('fontSize')) || DEFAULT_FONT_SIZE;
+    $body.css("font-size", savedFontSize + "px");
+
+    // Aumentar letra
+    $btnIncrease.on('click', function () {
+        let currentSize = parseInt($body.css("font-size"));
+        if (currentSize < MAX_FONT_SIZE) {
+            currentSize++;
+            $body.css("font-size", currentSize + "px");
+            localStorage.setItem("fontSize", currentSize);
+        }
+    });
+
+    // Disminuir letra
+    $btnDecrease.on('click', function () {
+        let currentSize = parseInt($body.css("font-size"));
+        if (currentSize > MIN_FONT_SIZE) {
+            currentSize--;
+            $body.css("font-size", currentSize + "px");
+            localStorage.setItem("fontSize", currentSize);
+        }
+    });
+
+
+
     // VALIDACIÓN DEL FORMULARIO
     $('#contact-form').validate({
         rules: {
@@ -66,15 +101,15 @@ $(document).ready(function () {
         },
 
         submitHandler: function (form) {
-            // Mostrar mensaje de éxito
             $('#success-message').fadeIn();
             
-            // Deshabilitar el botón para evitar reenvío
+            $(form)[0].reset();
+            
             $(form).find('button[type="submit"]').prop('disabled', true);
 
             console.log("Formulario enviado exitosamente (simulación).");
 
-            return false; // evita recargar la página
+            return false;
         }
     });
 
